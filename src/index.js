e66f9a39805a24b9aab367a46c06ec95c05247f4
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-19 06:53:31 +0900

    DOMの取得

--- src/index.js
+++ src/index.js
@@ -5,3 +5,15 @@ console.log(title1);
 //querySelectorを使う
 const title2 = document.querySelector("#title");
 console.log(title2);
+
+//クラス名でElementを取得
+// const containers = document.getElementsByClassName("container");
+// console.log(containers);
+
+//querySelectorによるElementの取得
+const container = document.querySelector(".container");
+console.log(container);
+
+//querySelectorAll
+const containers = document.querySelectorAll(".container");
+console.log(containers);
