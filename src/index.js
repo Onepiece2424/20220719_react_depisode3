ea97cef87a90f4b81147816b13c9584efb60364e
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-19 06:20:51 +0900

    id指定で要素を取得

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("test");
+//getElementByIdを使う
+const title1 = document.getElementById("title");
+console.log(title1);
+
+//querySelectorを使う
+const title2 = document.querySelector("#title");
+console.log(title2);
