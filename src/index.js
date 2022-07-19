7f9778b65d5334326dff20ffdf7917e3e53acc0a
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-19 13:57:03 +0900

    ボタンの設定

--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
-//getElementByIdを使う
-const title1 = document.getElementById("title");
-console.log(title1);
+// //getElementByIdを使う
+// const title1 = document.getElementById("title");
+// console.log(title1);
 
-//querySelectorを使う
-const title2 = document.querySelector("#title");
-console.log(title2);
+// //querySelectorを使う
+// const title2 = document.querySelector("#title");
+// console.log(title2);
 
-//クラス名でElementを取得
-// const containers = document.getElementsByClassName("container");
+// //クラス名でElementを取得
+// // const containers = document.getElementsByClassName("container");
+// // console.log(containers);
+
+// //querySelectorによるElementの取得
+// const container = document.querySelector(".container");
+// console.log(container);
+
+// //querySelectorAll
+// const containers = document.querySelectorAll(".container");
 // console.log(containers);
 
-//querySelectorによるElementの取得
-const container = document.querySelector(".container");
-console.log(container);
+// //divの生成
+// // const divEl = document.createElement("div");
+// // console.log(divEl);
 
-//querySelectorAll
-const containers = document.querySelectorAll(".container");
-console.log(containers);
-
-//divの生成
+// //DOMの追加
+// //div配下にpタグの生成
 // const divEl = document.createElement("div");
+
+// const pEl = document.createElement("p");
+
+// const h2El = document.createElement("h2");
+
+// // divEl.appendChild(pEl);
+// // divEl.appendChild(h2El);
+
+// divEl.prepend(pEl);
+// divEl.prepend(h2El);
 // console.log(divEl);
 
-//DOMの追加
-//div配下にpタグの生成
-const divEl = document.createElement("div");
+//ボタンの設定
+const buttonEl = document.createElement("button");
+buttonEl.textContent = "ボタン";
 
-const pEl = document.createElement("p");
-
-const h2El = document.createElement("h2");
-
-// divEl.appendChild(pEl);
-// divEl.appendChild(h2El);
-
-divEl.prepend(pEl);
-divEl.prepend(h2El);
-console.log(divEl);
+const divEl = document.querySelector(".container");
+divEl.appendChild(buttonEl);
